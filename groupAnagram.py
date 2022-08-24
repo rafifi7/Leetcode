b18a9388815a259ee4c24ff3c66de6080c289bb0
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2022-08-24 15:33:11 -0400

    Add files via upload

---
+++ groupAnagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        #create array of arrays
+        answer = []
+        #creates two hashmaps to be able to compare
+        word1, word2 = {}
+        #for loop that iterates through every word
+        for i in strs:
+            #for loop that iterates through every letter of every word
+            for j in i:
+                word1.set
+        
+        
+        #problem with idea is that you cannot add the word to a bunch of hashmaps.
