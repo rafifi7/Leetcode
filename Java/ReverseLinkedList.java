cae51cde26abd005dd4a700330608a06ae27dbb5
rafifi7 <rafeeadnan7@gmail.com>
2024-12-15 03:09:23 -0500

    fixed reverselinkedlist.java

--- Java/ReverseLinkedList.java
+++ Java/ReverseLinkedList.java
@@ -1,23 +1,33 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode curr = head; //makes a current ptr to the head node
-        ListNode prev = null; //prev is the previous ptr to current
-        while(curr != null) { 
-            ListNode temp = curr.next; //temp to store the next node of current
-            curr.next = prev; //next ptr is 
-            prev = curr;
-            curr = temp;
-        } 
-        return prev;
+        //base case for if linked list is smaller than 3 nodes
+        if (head == null || head.next == null) return head;
+
+        if (head.next.next == null) {
+            //swap
+            ListNode second = head.next;
+
+            second.next = head;
+            head.next = null;
+
+            return second;
+        }
+
+        //three pointer technique
+
+        ListNode left = null;
+        ListNode mid = head;
+        ListNode right = mid.next;
+
+        while (mid.next != null) {
+            mid.next = left; 
+
+            left = mid;
+            mid = right;
+            right = right.next;
+        }
+        mid.next = left;
+
+        return mid;
     }
 }
