4b0b4a959b11caf3876965c3475699701c87d9a1
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2025-10-29 00:59:59 -0400

    Two sum

---
+++ Python/Arrays & Hashing/twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        # one pass
+        for i in range(len(nums)):
+            num = nums[i]
+            if target - num in seen:
+                return [seen[target - num], i]
+            seen[num] = i
+        return None
