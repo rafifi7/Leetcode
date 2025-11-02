7d01f7ead505b2e9297e33eec264ca94b03032c8
Rafee Adnan <rafeeadnan7@gmail.com>
2025-11-02 18:05:17 -0500

    min stack

---
+++ Python/Stacks/minStack.py
@@ -0,0 +1,24 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None
+
+    def getMin(self) -> int:
+        if not self.stack:
+            return None
+        minimum = float("inf")
+        for i in self.stack:
+            minimum = min(i, minimum)
+        return minimum
+
