db34fe8869ff6b902d22dfd414060f19c380da00
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2025-11-12 00:24:51 -0500

    LRU Cache

--- Python/LinkedLists/LRUCache.py
+++ Python/LinkedLists/LRUCache.py
@@ -1,3 +1,8 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -6,24 +11,46 @@ class LRUCache:
         # a size field that gives us our size 
 
         # stores int key: int values
-        # we can make our recent keys a linked list of most recent to least recent
+        # we can make our recent keys a linked list of least recent to most recent
         # 1 <-> 2
 
         self.cache = {} 
-        self.recentKey = None
         self.size = capacity
 
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # remove from list
+    def remove(self, node):
+        temp = node.prev
+        temp.next = node.next
+        node.next.prev = temp
+
+    # insert from list
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.recentKey = key
-            return self.cache[key]
+            # we want to make it the most recent 
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if self.size == len(self.cache):
-            del self.cache[self.recentKey]
-        self.cache[key] = value
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.size:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
 
 
 # Your LRUCache object will be instantiated and called as such:
