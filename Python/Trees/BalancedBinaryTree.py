5198e6d3c9b9c2e873a2e65bea49459d15f99be5
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2025-11-20 15:10:16 -0500

    balanced binary tree comments

--- Python/Trees/BalancedBinaryTree.py
+++ Python/Trees/BalancedBinaryTree.py
@@ -10,21 +10,24 @@ from typing import Optional
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool: # type: ignore
         
-        # check if each sub tree is balanced
-
         def dfs(node):
-            if not node:
-                return (True, 0)
-
-            left, right = dfs(node.left), dfs(node.right)
-
-            # if left subtree and right subtree balanced 
-            #and the difference in height between both subtree is less than or equal to 1
-            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
             
-            return (balanced, 1 + max(left[1], right[1]))
+            if not node:
+                return [True, 0]
 
+            left = dfs(node.left)
+            right = dfs(node.right)
 
+            # we'll look at the subtrees heights to see if differences greater than 1
+            # and check if each subtree is balanced
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+
+            # want to return whether the current nodes subtree is balanced
+            # and get the maximum between the left height and right height to get the height ofthe subtree
+            return [balanced, max(left[1], right[1]) + 1]
+
+        # get the boolean value
         return dfs(root)[0]
-        
 
+            
+        
